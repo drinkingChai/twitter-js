b9350496e9e668f4b09d296218cd80229d35d310
Wasif Zaman <teatocode@gmail.com>
2017-07-12 00:23:36 -0400

    Complete part 3 of workshop

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require( 'express' );
 const chalk = require( 'chalk' );
-const app = express(); //creates an instande of an express application
+const app = express();
 
 
-app.listen(3000);
+app.use(function(req, res, next) {
+	console.log(chalk.cyan(req.method), chalk.magenta(req.url), chalk.blue(res.statusCode));
+	next();
+})
 
-app.use(function(req, res, next){
-	console.log(chalk.blue(req.body))
+
+app.get('/', function (req, res, next) {
 	next();
 });
 
-app.get('/', function (req, res){
-	res.send('Hello, guest!');
-});
+app.get('/special/', function(req, res, next) {
+	console.log(chalk.pink('you reached a special area'));
+	next();
+})
 
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log(chalk.green('listening on port ' + port));
+});
