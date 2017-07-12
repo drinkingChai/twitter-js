4ac9920a3e85e80a46ecb29b70428fcd5765258c
Wasif Zaman <teatocode@gmail.com>
2017-07-12 08:37:51 -0400

    Complete 5. Integration

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require( 'express' );
 const chalk = require( 'chalk' );
+const chokidar = require('chokidar');
+const nunjucks = require('nunjucks');
 const app = express();
+
+
+var locals = {
+	title: 'An Example',
+	people: [
+		{ name: 'Gandalf' },
+		{ name: 'Frodo' },
+		{ name: 'Hermoine' }
+	]
+}
+
+// nunjucks.render('index.html', locals, , function(err, log) {
+// 	console.log(log);
+// })
+
+app.set('view engine', 'html');
+app.engine('htm', nunjucks.render);
+nunjucks.configure('views', {
+	// nunjucks.configure must come after app.engine & app.set
+	express: app,
+	noCache: true
+})
 
 
 app.use(function(req, res, next) {
@@ -10,11 +34,12 @@ app.use(function(req, res, next) {
 
 
 app.get('/', function (req, res, next) {
+	res.render('index.html', locals);
 	next();
 });
 
 app.get('/special/', function(req, res, next) {
-	console.log(chalk.pink('you reached a special area'));
+	console.log('you reached a special area');
 	next();
 })
 
