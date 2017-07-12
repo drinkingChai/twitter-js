64e2e9e03d64445133425655e4e61be40320c7ce
Wasif Zaman <teatocode@gmail.com>
2017-07-12 17:29:28 -0400

    Add some style to index and users route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,13 @@ const tweetBank = require('../tweetBank');
 router.get('/', function (req, res) {
 	// res.render calls next automatically
   let tweets = tweetBank.list();
-	res.render('index', { tweets: tweets});
+	res.render('index', { tweets: tweets });
 });
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find({ name: name });
+  res.render('index', { list: list });
+})
 
 module.exports = router;
