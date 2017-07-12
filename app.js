a7b2e75186aca691cdaaeb5af1aee08d2d980e8d
Mitch Schindler <mitch@mitchschindler.com>
2017-07-11 21:51:57 -0400

    after adding chalk

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require( 'express' );
+const chalk = require( 'chalk' );
 const app = express(); //creates an instande of an express application
+
 
 app.listen(3000);
 
+app.use(function(req, res, next){
+	console.log(chalk.blue(req.body))
+	next();
+});
+
 app.get('/', function (req, res){
-	res.send('Hello, guest!')
-})
+	res.send('Hello, guest!');
+});
+
